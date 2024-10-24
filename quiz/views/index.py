7229654265bf2cff4c7b34bf1937537c58a16c46
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-10-24 17:59:16 +0700

    Add Auth View

--- quiz/views/index.py
+++ quiz/views/index.py
@@ -19,6 +19,22 @@ def register_view(request):
         password = request.POST.get('password')
         role = 'student'
 
+        if CustomUser.objects.filter(username=username).exists():
+            messages.error(request, 'Username đã tồn tại')
+            return redirect('register')
+        if CustomUser.objects.filter(email=email).exists():
+            messages.error(request, 'Email đã tồn tại')
+            return redirect('register')
+        
+        user = CustomUser.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            role=role
+        )
+
+        return redirect('login')
+
 
     return render(request, 'quiz/register.html', status=200)
 
@@ -30,6 +46,13 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Sai tên đăng nhập hoặc mật khẩu')
     
     return render(request, 'quiz/login.html', status=200)
 

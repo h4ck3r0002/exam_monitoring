aeb327263a4ce348106a747b85518a2d3c1dd22a
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-11-04 14:15:17 +0700

    Fix model 1019

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 Django, OpenCV
 <br />
-website for monitoring and preventing exam fraud 
-
-subprocess xu ly gian lan khi call (done)
-cau hoi trac nghiem dang chon dung sai va lua chon (done)
-await xu ly o client 
-api check 
-
-xu ly bai kiem tra de moi nguoi chi thi mot lan duy nhat 
-neu thi roi bao da thi roi, neu trong thoi gian thi moi cho phep thi 
-het 15p thi tu dong dung
-viet phan cho phep xem lich su thi 
+website for monitoring and preventing exam fraud <br/>
+<br/>
+subprocess xu ly gian lan khi call (done) <br/>
+cau hoi trac nghiem dang chon dung sai va lua chon (done)<br/>
+await xu ly o client <br/>
+api check <br/>
+<br/>
+xu ly bai kiem tra de moi nguoi chi thi mot lan duy nhat <br/>
+neu thi roi bao da thi roi, neu trong thoi gian thi moi cho phep thi <br/>
+het 15p thi tu dong dung <br/>
+viet phan cho phep xem lich su thi <br/>
--- handle_cheat.py
+++ handle_cheat.py
@@ -4,200 +4,22 @@ import os
 import django
 import numpy as np
 from django.conf import settings
-from collections import deque
+from collections import deque 
+import tensorflow as tf
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exam_monitoring.settings')
 django.setup()
 
 from quiz.models.quiz import Monitor, Result
 
-
-class SCRFD():
-    def __init__(self, onnxmodel, confThreshold=0.5, nmsThreshold=0.5):
-        self.inpWidth = 640
-        self.inpHeight = 640
-        self.confThreshold = confThreshold
-        self.nmsThreshold = nmsThreshold
-        self.net = cv2.dnn.readNet(onnxmodel)
-        #self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-        #self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-        self.keep_ratio = True
-        self.fmc = 3
-        self._feat_stride_fpn = [8, 16, 32]
-        self._num_anchors = 2
-    def resize_image(self, srcimg):
-        padh, padw, newh, neww = 0, 0, self.inpHeight, self.inpWidth
-        if self.keep_ratio and srcimg.shape[0] != srcimg.shape[1]:
-            hw_scale = srcimg.shape[0] / srcimg.shape[1]
-            if hw_scale > 1:
-                newh, neww = self.inpHeight, int(self.inpWidth / hw_scale)
-                img = cv2.resize(srcimg, (neww, newh), interpolation=cv2.INTER_AREA)
-                padw = int((self.inpWidth - neww) * 0.5)
-                img = cv2.copyMakeBorder(img, 0, 0, padw, self.inpWidth - neww - padw, cv2.BORDER_CONSTANT,
-                                         value=0)  # add border
-            else:
-                newh, neww = int(self.inpHeight * hw_scale) + 1, self.inpWidth
-                img = cv2.resize(srcimg, (neww, newh), interpolation=cv2.INTER_AREA)
-                padh = int((self.inpHeight - newh) * 0.5)
-                img = cv2.copyMakeBorder(img, padh, self.inpHeight - newh - padh, 0, 0, cv2.BORDER_CONSTANT, value=0)
-        else:
-            img = cv2.resize(srcimg, (self.inpWidth, self.inpHeight), interpolation=cv2.INTER_AREA)
-        return img, newh, neww, padh, padw
-    def distance2bbox(self, points, distance, max_shape=None):
-        x1 = points[:, 0] - distance[:, 0]
-        y1 = points[:, 1] - distance[:, 1]
-        x2 = points[:, 0] + distance[:, 2]
-        y2 = points[:, 1] + distance[:, 3]
-        if max_shape is not None:
-            x1 = x1.clamp(min=0, max=max_shape[1])
-            y1 = y1.clamp(min=0, max=max_shape[0])
-            x2 = x2.clamp(min=0, max=max_shape[1])
-            y2 = y2.clamp(min=0, max=max_shape[0])
-        return np.stack([x1, y1, x2, y2], axis=-1)
-    def distance2kps(self, points, distance, max_shape=None):
-        preds = []
-        for i in range(0, distance.shape[1], 2):
-            px = points[:, i % 2] + distance[:, i]
-            py = points[:, i % 2 + 1] + distance[:, i + 1]
-            if max_shape is not None:
-                px = px.clamp(min=0, max=max_shape[1])
-                py = py.clamp(min=0, max=max_shape[0])
-            preds.append(px)
-            preds.append(py)
-        return np.stack(preds, axis=-1)
-    def detect(self, srcimg):
-        img, newh, neww, padh, padw = self.resize_image(srcimg)
-        blob = cv2.dnn.blobFromImage(img, 1.0 / 128, (self.inpWidth, self.inpHeight), (127.5, 127.5, 127.5), swapRB=True)
-        # Sets the input to the network
-        self.net.setInput(blob)
-
-        # Runs the forward pass to get output of the output layers
-        outs = self.net.forward(self.net.getUnconnectedOutLayersNames())
-        # inference output
-        scores_list, bboxes_list, kpss_list = [], [], []
-        for idx, stride in enumerate(self._feat_stride_fpn):
-            scores = outs[idx * self.fmc][0]
-            bbox_preds = outs[idx * self.fmc + 1][0] * stride
-            kps_preds = outs[idx * self.fmc + 2][0] * stride
-            height = blob.shape[2] // stride
-            width = blob.shape[3] // stride
-            anchor_centers = np.stack(np.mgrid[:height, :width][::-1], axis=-1).astype(np.float32)
-            anchor_centers = (anchor_centers * stride).reshape((-1, 2))
-            if self._num_anchors > 1:
-                anchor_centers = np.stack([anchor_centers] * self._num_anchors, axis=1).reshape((-1, 2))
-
-            pos_inds = np.where(scores >= self.confThreshold)[0]
-            bboxes = self.distance2bbox(anchor_centers, bbox_preds)
-            pos_scores = scores[pos_inds]
-            pos_bboxes = bboxes[pos_inds]
-            scores_list.append(pos_scores)
-            bboxes_list.append(pos_bboxes)
-
-            kpss = self.distance2kps(anchor_centers, kps_preds)
-            kpss = kpss.reshape((kpss.shape[0], -1, 2))
-            pos_kpss = kpss[pos_inds]
-            kpss_list.append(pos_kpss)
-
-        scores = np.vstack(scores_list).ravel()
-        # bboxes = np.vstack(bboxes_list) / det_scale
-        # kpss = np.vstack(kpss_list) / det_scale
-        bboxes = np.vstack(bboxes_list)
-        kpss = np.vstack(kpss_list)
-        bboxes[:, 2:4] = bboxes[:, 2:4] - bboxes[:, 0:2]
-        ratioh, ratiow = srcimg.shape[0] / newh, srcimg.shape[1] / neww
-        bboxes[:, 0] = (bboxes[:, 0] - padw) * ratiow
-        bboxes[:, 1] = (bboxes[:, 1] - padh) * ratioh
-        bboxes[:, 2] = bboxes[:, 2] * ratiow
-        bboxes[:, 3] = bboxes[:, 3] * ratioh
-        kpss[:, :, 0] = (kpss[:, :, 0] - padw) * ratiow
-        kpss[:, :, 1] = (kpss[:, :, 1] - padh) * ratioh
-        indices = cv2.dnn.NMSBoxes(bboxes.tolist(), scores.tolist(), self.confThreshold, self.nmsThreshold)
-        
-        '''
-        for i in indices:
-            #i = i[0]
-            xmin, ymin, xamx, ymax = int(bboxes[i, 0]), int(bboxes[i, 1]), int(bboxes[i, 0] + bboxes[i, 2]), int(bboxes[i, 1] + bboxes[i, 3])
-            cv2.rectangle(srcimg, (xmin, ymin), (xamx, ymax), (0, 0, 255), thickness=2)
-            for j in range(5):
-                cv2.circle(srcimg, (int(kpss[i, j, 0]), int(kpss[i, j, 1])), 1, (0,255,0), thickness=-1)
-            cv2.putText(srcimg, str(round(scores[i], 3)), (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), thickness=1)
-        '''
-        return bboxes[indices], kpss[indices], scores[indices]#indices#srcimg     
-
-def are_coordinates_in_frame(frame, box, pts):
-    """
-    Parameters
-    ----------
-    frame : uint8
-        RGB image (numpy array).
-    bbs : float64
-        coordinates of bounding box.
-    points : flaot32
-        coordinates of landmarks.
-
-    Returns
-    -------
-    boolean
-    """
-    
-    height, width = frame.shape[:2]
-    
-    if np.any(box <= 0) or np.any(box >= height) or np.any(box >= width):
-        return False
-    if np.any(pts <= 0) or np.any(pts >= height) or np.any(pts >= width):
-        return False
-    
-    return True
-
-def find_pose(points):
-    """
-    Parameters
-    ----------
-    points : float32, Size = (5,2)
-        coordinates of landmarks for the selected faces.
-    Returns
-    -------
-    float32, float32, float32
-    """
-    LMx = points[:,0]#points[0:5]# horizontal coordinates of landmarks
-    LMy = points[:,1]#[5:10]# vertical coordinates of landmarks
-    
-    dPx_eyes = max((LMx[1] - LMx[0]), 1)
-    dPy_eyes = (LMy[1] - LMy[0])
-    angle = np.arctan(dPy_eyes / dPx_eyes) # angle for rotation based on slope
-    
-    alpha = np.cos(angle)
-    beta = np.sin(angle)
-    
-    # rotated landmarks
-    LMxr = (alpha * LMx + beta * LMy + (1 - alpha) * LMx[2] / 2 - beta * LMy[2] / 2) 
-    LMyr = (-beta * LMx + alpha * LMy + beta * LMx[2] / 2 + (1 - alpha) * LMy[2] / 2)
-    
-    # average distance between eyes and mouth
-    dXtot = (LMxr[1] - LMxr[0] + LMxr[4] - LMxr[3]) / 2
-    dYtot = (LMyr[3] - LMyr[0] + LMyr[4] - LMyr[1]) / 2
-    
-    # average distance between nose and eyes
-    dXnose = (LMxr[1] - LMxr[2] + LMxr[4] - LMxr[2]) / 2
-    dYnose = (LMyr[3] - LMyr[2] + LMyr[4] - LMyr[2]) / 2
-    
-    # relative rotation 0 degree is frontal 90 degree is profile
-    Xfrontal = (-90+90 / 0.5 * dXnose / dXtot) if dXtot != 0 else 0
-    Yfrontal = (-90+90 / 0.5 * dYnose / dYtot) if dYtot != 0 else 0
-
-    return angle * 180 / np.pi, Xfrontal, Yfrontal
-
-
-# Load mô hình SCRFD
-onnxmodel = 'scrfd_2.5g_kps.onnx'
-confThreshold = 0.5
-nmsThreshold = 0.5
-face_detector = SCRFD(onnxmodel, confThreshold=confThreshold, nmsThreshold=nmsThreshold)
-
+# Load the TensorFlow model
+model = tf.keras.models.load_model("model.savedmodel") 
+class_names = open("labels.txt", "r").readlines()
 
 def process_video(monitor_id):
     reason = ''
-    
+
     # Lấy đối tượng Monitor
     monitor = Monitor.objects.get(id=monitor_id)
     video_path = monitor.video.path
@@ -211,22 +33,38 @@ def process_video(monitor_id):
         if not ret:
             break
 
-        # Phát hiện khuôn mặt sử dụng SCRFD
-        bboxes = face_detector.detect(frame)
-        
-        # Kiểm tra nếu không có người trong camera
-        if len(bboxes) == 0:
-            print("Không có người xuất hiện trong camera.")
-            reason = 'Không có người xuất hiện trong camera.'
-            is_cheat = True
-            break
-        elif len(bboxes) > 1:
-            print("Phát hiện nhiều hơn 1 người trong camera.")
-            is_cheat = True
-            reason = 'Phát hiện nhiều hơn 1 người trong camera.'
-            break
+        # Resize and preprocess frame for the model
+        image = cv2.resize(frame, (224, 224))
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+        image = (image / 127.5) - 1  # Normalize as per model's input requirement
+
+        # Predict cheating status using the model
+        prediction = model.predict(image)
+
+        # # Assuming the model outputs a binary classification (0: no cheating, 1: cheating)
+        # if prediction[0][0] > 0.5:  # Adjust threshold if necessary
+        #     print("Cheating detected.")
+        #     is_cheat = True
+        #     reason = 'Detected cheating.'
+        #     break
+
+        # Kiểm tra cấu trúc đầu ra
+        if isinstance(prediction, dict):
+            output_key = list(prediction.keys())[0]  # Lấy tên của tensor đầu ra
+            prediction = prediction[output_key]  # Lấy tensor từ từ điển
+
+        # Tìm lớp dự đoán và điểm số độ tin cậy
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
+
+        if class_name == 'Cheat':
+            is_cheat = True 
+            reason = 'Detected cheating (Không có mặt trong màn hình, quay trái phải hoặc có nhiều người trong khung hình)'
+            break 
 
     camera.release()
+
 
     # Cập nhật trạng thái gian lận trong cơ sở dữ liệu
     monitor.is_cheat = is_cheat
@@ -245,6 +83,3 @@ def process_video(monitor_id):
 if __name__ == "__main__":
     monitor_id = int(sys.argv[1])
     process_video(monitor_id)
-
-
-

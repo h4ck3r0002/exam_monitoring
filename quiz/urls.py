b4dbf051b3a5ca48a4d659e04d71642dc01230f2
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-11-14 15:46:38 +0700

    Lock Register

--- quiz/urls.py
+++ quiz/urls.py
@@ -7,7 +7,7 @@ from quiz.views.monitor import *
 urlpatterns = [
     path('', index_view, name='index'),
     path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
+    # path('register/', register_view, name='register'),
     path('logout/', logout_view, name='logout'),
     path('exam/<pk>', exam_view, name='exam'),
     path('result/<pk>', result_view, name='result'),
